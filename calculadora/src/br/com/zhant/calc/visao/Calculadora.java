397cd56bd07a5f60b75f502352cae6c35ffdc8fb
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-19 12:05:34 -0300

    Criando Janela da calculadora

--- calculadora/src/br/com/zhant/calc/visao/Calculadora.java
+++ calculadora/src/br/com/zhant/calc/visao/Calculadora.java
@@ -1,5 +1,19 @@
 package br.com.zhant.calc.visao;
 
-public class Calculadora {
+import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame{
+
+	public Calculadora() {
+		setVisible(true);//Seta visibilidade como verdadeira, criando assim o "app desktop"	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);//Encerra o processo ao fechar a pagina
+		setSize(233,322);//Define o tamanho
+		setLocationRelativeTo(null);//faz com que o app seja aberto no meio da tela
+	}
+	
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+	
 }
