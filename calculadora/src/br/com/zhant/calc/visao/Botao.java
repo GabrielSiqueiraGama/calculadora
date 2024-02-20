1b59204f4e3c6348656e00994019a7ed30870066
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-20 12:05:24 -0300

    Criando botões.

--- calculadora/src/br/com/zhant/calc/visao/Botao.java
+++ calculadora/src/br/com/zhant/calc/visao/Botao.java
@@ -1,8 +1,21 @@
 package br.com.zhant.calc.visao;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class Botao extends JButton{
 
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("courier", Font.PLAIN, 25));
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+	
 }
--- calculadora/src/br/com/zhant/calc/visao/Teclado.java
+++ calculadora/src/br/com/zhant/calc/visao/Teclado.java
@@ -1,11 +1,18 @@
 package br.com.zhant.calc.visao;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel{
 
+	private final Color COR_CINZA_ESCURO = new Color(68,68,68);
+	private final Color COR_CINZA_CLARO = new Color(99,99,99);
+	private final Color COR_LARANJA = new Color(242,163,60);
+	
 	public Teclado() {
+		
 	}
 	
 	
