e175801692d5f7c6ea17c1ac6dcf03b1f51c1400
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-02-21 12:09:50 -0300

    Finalizando interface grafica.

--- calculadora/src/br/com/zhant/calc/visao/Teclado.java
+++ calculadora/src/br/com/zhant/calc/visao/Teclado.java
@@ -18,20 +18,25 @@ public class Teclado extends JPanel{
 		GridBagLayout layout = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
 		setLayout(layout);
+		
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		
 		//Linha 01
 		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0,0);
 		adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1,0);
 		adicionarBotao("%", COR_CINZA_ESCURO, c, 2,0);
 		adicionarBotao("+", COR_LARANJA, c, 3,0);
 		//Linha 02
-		adicionarBotao("1", COR_CINZA_ESCURO, c, 0,1);
-		adicionarBotao("2", COR_CINZA_ESCURO, c, 1,1);
-		adicionarBotao("3", COR_CINZA_ESCURO, c, 2,1);
+		adicionarBotao("1", COR_CINZA_CLARO, c, 0,1);
+		adicionarBotao("2", COR_CINZA_CLARO, c, 1,1);
+		adicionarBotao("3", COR_CINZA_CLARO, c, 2,1);
 		adicionarBotao("-", COR_LARANJA, c, 3,1);
 		//Linha 03
-		adicionarBotao("4", COR_CINZA_ESCURO, c, 0,2);
-		adicionarBotao("5", COR_CINZA_ESCURO, c, 1,2);
-		adicionarBotao("6", COR_CINZA_ESCURO, c, 2,2);
+		adicionarBotao("4", COR_CINZA_CLARO, c, 0,2);
+		adicionarBotao("5", COR_CINZA_CLARO, c, 1,2);
+		adicionarBotao("6", COR_CINZA_CLARO, c, 2,2);
 		adicionarBotao("*", COR_LARANJA, c, 3,2);
 		//Linha 04
 		adicionarBotao("7", COR_CINZA_CLARO, c, 0,3);
@@ -39,8 +44,9 @@ public class Teclado extends JPanel{
 		adicionarBotao("9", COR_CINZA_CLARO, c, 2,3);
 		adicionarBotao("/", COR_LARANJA, c, 3,3);
 		//Linha 05
+		c.gridwidth = 2;
 		adicionarBotao("0", COR_CINZA_CLARO, c, 0,4);
-		adicionarBotao(".", COR_CINZA_CLARO, c, 1,4);
+		c.gridwidth = 1;
 		adicionarBotao(".", COR_CINZA_CLARO, c, 2,4);
 		adicionarBotao("=", COR_LARANJA, c, 3,4);
 	}
